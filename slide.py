16dcd1d75de4f64817863fb5f504be1cd9c28ec1
tmuytten <thomas.uyttenhove@ugent.be>
2019-02-28 19:16:44 +0100

    Tags in slides

--- slide.py
+++ slide.py
@@ -1,3 +1,5 @@
+from photo import Photo
+
 class Slide:
 	"""A slide"""
 
@@ -5,9 +7,17 @@ class Slide:
 		self.photo1 = photo1
 		self.photo2 = photo2
 
+		self.tags = []
+		self.tags.extend(photo1.tags)
+
+		if photo2 is not None:
+			self.tags.extend(photo2.tags)
+
 	def __str__(self):
 		if self.photo2 is None:
 			return f'{self.photo1}\n'
 		else:
 			return f'{self.photo1} {self.photo2}\n'
-
+			
+	def getTags(self):
+		return self.tags
