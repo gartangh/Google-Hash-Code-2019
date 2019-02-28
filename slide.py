fd538bfde83b92408ba0b75be945854af6a804cc
tmuytten <thomas.uyttenhove@ugent.be>
2019-02-28 19:29:07 +0100

    getScore

--- slide.py
+++ slide.py
@@ -15,9 +15,16 @@ class Slide:
 
 	def __str__(self):
 		if self.photo2 is None:
-			return f'{self.photo1}\n'
+			return f'{self.photo1.id}\n'
 		else:
-			return f'{self.photo1} {self.photo2}\n'
+			return f'{self.photo1.id} {self.photo2.id}\n'
 			
 	def getTags(self):
 		return self.tags
+
+	@staticmethod
+	def getScore(slide1, slide2):
+		score1 = len(slide1.tags & slide2.tags)
+		score2 = len(slide1.tags) - len(slide1.tags & slide2.tags)
+		score3 = len(slide2.tags) - len(slide1.tags & slide2.tags)
+		return min(score1, score2, score3)
