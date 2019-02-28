3665cc4c6d870ea9916db3370ef4b3477583a960
Garben Tanghe <garben.tanghe@gmail.com>
2019-02-28 18:56:42 +0100

    added input

---
+++ main.py
@@ -0,0 +1,26 @@
+# imports
+import numpy as np
+import photo
+
+
+# main
+if __name__ == '__main__':
+
+	photos = []
+
+	with open('in/a_example.in') as file_in:
+		N = int(file_in.readline().split()[0])
+
+		for i in range(N):
+			line = file_in.readline().split()
+			tags = []
+			for j in range(line[1]):
+				tags.append(line[j+2])
+			photos.append(Photo(i, line[0], tags))
+
+	# TODO create algo
+
+	with open('out/a_example.txt', 'w') as file_out:
+		file_out.write(f'{len()}\n')
+		for  in :
+			file_out.write(str(slice))
